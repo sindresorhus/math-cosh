27113630258295667cf2c7a6ea4c756afcbd4b98
Sindre Sorhus <sindresorhus@gmail.com>
2016-01-01 00:08:49 +0100

    update tests for latest AVA version

--- test.js
+++ test.js
@@ -1,22 +1,21 @@
-'use strict';
-var test = require('ava');
-var numberIsNan = require('number-is-nan');
-var floatEqual = require('float-equal');
-Math.cosh = undefined;
-var cosh = require('./');
+import test from 'ava';
+import numberIsNan from 'number-is-nan';
+import floatEqual from 'float-equal';
 
-test(function (t) {
-	t.assert(numberIsNan(cosh(NaN)));
-	t.assert(cosh(+0) === 1);
-	t.assert(cosh(-0) === 1);
-	t.assert(cosh(Infinity) === Infinity);
-	t.assert(cosh(-Infinity) === -Infinity);
-	t.assert(cosh(0) === 1);
-	t.assert(cosh(1) === 1.5430806348152437);
-	t.assert(cosh(-1) === 1.5430806348152437);
-	t.assert(floatEqual(cosh(12), 81377.39571257404));
-	t.assert(floatEqual(cosh(22), 1792456423.065798));
-	t.assert(floatEqual(cosh(-10), 11013.23292010332));
-	t.assert(floatEqual(cosh(-23), 4872401723.124454));
-	t.end();
+Math.cosh = undefined;
+const cosh = require('./');
+
+test(t => {
+	t.true(numberIsNan(cosh(NaN)));
+	t.is(cosh(+0), 1);
+	t.is(cosh(-0), 1);
+	t.is(cosh(Infinity), Infinity);
+	t.is(cosh(-Infinity), -Infinity);
+	t.is(cosh(0), 1);
+	t.is(cosh(1), 1.5430806348152437);
+	t.is(cosh(-1), 1.5430806348152437);
+	t.true(floatEqual(cosh(12), 81377.39571257404));
+	t.true(floatEqual(cosh(22), 1792456423.065798));
+	t.true(floatEqual(cosh(-10), 11013.23292010332));
+	t.true(floatEqual(cosh(-23), 4872401723.124454));
 });
